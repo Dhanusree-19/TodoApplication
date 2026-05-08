bc994ead9395e71fbc3332a27a9385dc974484e2
dhanusree-19 <dhanudhanu1903@gmail.com>
2026-05-08 18:14:29 +0530

    Todo create, rename, delete

--- src/main/java/com/example/alter/entity/User.java
+++ src/main/java/com/example/alter/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.alter.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-@Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,4 +20,28 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
